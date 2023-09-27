fb589b9ead44c67b21e5df3d7efdbd4d0aa89f3d
Arash <45104106+arashHaratian@users.noreply.github.com>
2023-09-27 23:46:46 +0200

    first implementation of value net

---
+++ ai_academy/draft/value_net.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+from tensorflow import keras
+from keras.models import Model
+from keras import layers
+from keras.optimizers import RMSprop, Adam
+from keras.utils import pad_sequences
+
+
+
+def Value_net(
+        # state_seq,
+        seq_len,
+        action,
+        n_actions: int, 
+        n_features: int,
+        height: int = 21, 
+        width: int = 21, 
+        depth: int = 11,
+        hidden_dim = 64
+    ):
+
+    ## StateSeqEmb class in the original code 
+    state_input = layers.Input(shape=(n_features,))
+    goal_input = layers.Input(shape=(n_features,))
+    state_seq_input = layers.Input(shape=(seq_len,))
+    embed = layers.Embedding(n_features + 1, hidden_dim, mask_zero = True)(state_seq_input)
+    padded_embed = pad_sequences(embed, padding='post')
+    x_rnn = layers.StackedRNNCells(
+        [layers.GRU(hidden_dim),
+        layers.GRU(hidden_dim),
+        layers.GRU(hidden_dim)])(padded_embed)
+    
+    x = tf.concat([x_rnn, action], axis=1)
+
+    ## Vanilla Value net class in the original code 
+    x = layers.Dense(hidden_dim, activation='relu')(x)
+    x = layers.Dense(hidden_dim, activation='relu')(x)
+    x = layers.Dense(hidden_dim, activation='relu')(x)
+    value = layers.Dense(1, activation='linear')(x)
+
+    model = Model([state_input, goal_input, state_seq_input], value)
+    model.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
+
+    return model
+
+    
