4be8db02bf84f98f858e5b442bb656f58b224dc9
Arash <45104106+arashHaratian@users.noreply.github.com>
2023-09-27 23:46:31 +0200

    draft policy net

--- ai_academy/draft/policy_net.py
+++ ai_academy/draft/policy_net.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 from tensorflow import keras
 from keras.models import Model
 from keras import layers
@@ -18,24 +19,33 @@ def Policy_net(
     ):
 
 
-    state_input = layers.Input(shape=(n_features,))
-    goal_input = layers.Input(shape=(n_features,))
-    map_input = layers.Input(shape=(height, width, depth))
-    input_data = layers.Concatenate()([state_input, goal_input])
-    
     ## StateSeqEmb class in the original code 
-    input_data = pad_sequences(input_data, padding='post')
-    embed = layers.Embedding(input_data.shape[0] + 1, hidden_dim, mask_zero = True)(input_data)
-    x = layers.GRU(hidden_dim)(embed)
-    x = layers.GRU(hidden_dim)(x)
-    x = layers.GRU(hidden_dim)(x)
+    embed = layers.Embedding(n_features + 1, hidden_dim, mask_zero = True)(seq_len)
+    padded_embed = pad_sequences(embed, padding='post')
+    x_rnn = layers.StackedRNNCells(
+        [layers.GRU(hidden_dim),
+        layers.GRU(hidden_dim),
+        layers.GRU(hidden_dim)])(padded_embed)
 
+    ## Vanilla Policy class in the original code 
+    x = layers.Dense(hidden_dim, activation='relu')(x_rnn)
+    x = layers.Dense(hidden_dim, activation='relu')(x)
+    x = layers.Dense(hidden_dim, activation='relu')(x)
+    x = layers.Dense(n_actions, activation='linear')(x)
 
-    x = layers.Dense(hidden_dim, activation='relu')(x)
-    x = layers.Dense(hidden_dim, activation='relu')(x)
-    x = layers.Dense(hidden_dim, activation='relu')(x)
-    output = layers.Dense(n_actions, activation='relu')(x)
-    model = Model(input_data, output)
-    model.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
+    last_states = state_seq[tf.range(state_seq.shape[0]), seq_len-1]
+    action_domain = tf.zeros((n_features, n_actions)) # should put to 1 for terminal state
+
+    output = tf.where(tf.cast(1-action_domain[last_states], tf.bool), 1e-32, x)
+    # output = layers.Dense(n_actions, activation='relu')(x)
+    prob = layers.Softmax(axis=1)(output)
+    action_dist = layers.DistributionLambda(
+        lambda p: tfp.distributions.Categorical(p))(prob)
+    model = Model(input_data, action_dist)
+
+    model.compile(optimizer=Adam(learning_rate=0.01), loss='loss_policy')
+
     return model
 
+def policy_loss():
+    pass
