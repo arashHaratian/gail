be8aff0ee78e606c740e5105ed28a339e05600a2
Arash Haratian <9359-araha13@users.noreply.gitlab.liu.se>
2023-11-01 15:43:41 +0100

    getting the expert data

--- ai_academy/temp.py
+++ ai_academy/temp.py
@@ -5,13 +5,14 @@ from draft.value_net import Value_net
 from draft.training import *
 from maze_env import Maze
 from environment import get_obstacles
+from bi_a_star_2 import main_bi_astar
 import itertools
 import random
 
 batch = 2048
 n_space = 3
 n_actions = 6
-max_len = 20
+max_len = 100
 num_trajs = batch+10
 
 
@@ -19,7 +20,7 @@ env_dim = tf.constant([40, 40, 6])
 
 n_features = 40 + 1
 
-## Setting up the env
+## ========================== Setting up the env ==========================
 obstacles, obstacles_x, obstacles_y, obstacles_z = get_obstacles()
 env = Maze(obstacles)
 
@@ -33,26 +34,25 @@ end_state = tf.reshape(env.end_node, (1, -1))
 goal_inputs_train  = tf.repeat(end_state, num_trajs, 0)
 
 
-## TODO use bi_a_star_2.py
-y_train_action = tf.random.uniform((num_trajs, n_actions))
-
-
+## ========================== Creating the networks ==========================
 discrim = Discrim_net(n_actions, n_features) 
 policy = Policy_net(n_actions, n_features)
 value = Value_net(n_actions, n_features)
 
+
+## ========================== Collecting the learner trajs ==========================
 learner_observations, learner_actions, learner_len, learner_rewards =unroll_traj(state_inputs_train, goal_inputs_train,
                                                                                 env, policy,
                                                                                 batch, num_trajs, max_len)
 
 
 
-S = learner_len.sum()
-M = learner_len.max()
+S_learner = learner_len.sum()
+M_learner = learner_len.max()
 
-learner_obs = tf.zeros((S, M, n_space)).numpy() ## zeros since we mask them in RNN ##TODO problem of cordinate zero!
-learner_act = tf.zeros((S)).numpy()
-learner_l = tf.zeros((S)).numpy()
+learner_obs = tf.zeros((S_learner, M_learner, n_space)).numpy() ## zeros since we mask them in RNN
+learner_act = tf.zeros((S_learner)).numpy()
+learner_l = tf.zeros((S_learner)).numpy()
 cnt = 0
 
 ## TODO: needs to be check (just copied and added the last dimension, not sure if it works)
@@ -67,8 +67,45 @@ for sample in range(num_trajs):
             # print("break with index error in Learner Trajectory")
             break
 
-expert_observations, expert_actions, expert_len = expert_data
 
+
+## ========================== Collecting the expert (b-star) trajs ==========================
+expert_observations = tf.zeros((num_trajs, max_len, 3)).numpy()
+expert_actions = tf.zeros((num_trajs, max_len)).numpy()
+expert_len = tf.zeros((num_trajs), dtype=tf.dtypes.int32).numpy()
+
+for idx, (start_point, end_point) in enumerate(zip(state_inputs_train, goal_inputs_train)):
+    traj, action, _ = main_bi_astar(defined_yaml = False,
+                                    options = ['rain_forest', 17],
+                                    user_start_node = start_point, user_goal_node = end_point)
+    traj_len = len(traj)
+    expert_observations[idx, :traj_len, :] = traj
+    expert_actions[idx, :traj_len - 1] = action
+    expert_len[idx] = traj_len
+
+
+S_expert = expert_len.sum()
+M_expert = expert_len.max()
+
+expert_obs = tf.zeros((S_expert, M_expert, n_space)).numpy() ## zeros since we mask them in RNN 
+expert_act = tf.zeros((S_expert)).numpy()
+expert_l = tf.zeros((S_expert)).numpy()
+cnt = 0
+
+## TODO: needs to be check (just copied and added the last dimension, not sure if it works)
+for sample in range(num_trajs):
+    for seq_length in range(1, expert_len[sample]+1):
+        try:
+            expert_obs[cnt, :seq_length, :] = expert_observations[sample, :seq_length, :]
+            expert_act[cnt] = int(expert_actions[sample][seq_length-1])
+            expert_l[cnt] = seq_length
+            cnt += 1
+        except:
+            # print("break with index error in expert Trajectory")
+            break
+
+
+## ========================== Train using the collected trajs ==========================
 train(policy, value, discrim,
     learner_obs, learner_act, learner_len,
     expert_observations, expert_actions, expert_len,
